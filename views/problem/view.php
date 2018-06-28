9ad9a4e4ca7896416a39193331e82d459c54c079
jojeringnax <hollow718@gmail.com>
2018-06-28 17:31:46 +0200

    Adding CRUD Problems fix

--- views/problem/view.php
+++ views/problem/view.php
@@ -32,6 +32,8 @@ $this->params['breadcrumbs'][] = $this->title;
             'category',
             'place',
             'comment',
+            'room_id',
+            'status'
         ],
     ]) ?>
 

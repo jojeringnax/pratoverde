2a986410aff23b99d9e9af494b33f9ea43ae77e8
jojeringnax <hollow718@gmail.com>
2018-06-29 11:52:38 +0200

    Adding CRUD Problems fix

---
+++ models/Log.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jojer
+ * Date: 29/06/2018
+ * Time: 11:37
+ */
+
+namespace app\models;
+
+
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "logs".
+ *
+ * Class Log
+ * @package app\models
+ *
+ * @property int $id
+ * @property string $text
+ */
+class Log extends ActiveRecord
+{
+
+    public static function write($message)
+    {
+        $log = new self;
+        $log->text = $message;
+        return $log->save();
+    }
+
+
+}

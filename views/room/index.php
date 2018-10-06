ee152d08b0d5f6e5d35cbdb59140dfd16e29102f
jojeringnax <hollow718@gmail.com>
2018-10-06 12:02:23 +0200

    All front-end (apprx)

--- views/room/index.php
+++ views/room/index.php
@@ -8,8 +8,11 @@ $this->registerCssFile('/css/room.css', ['depends' => [\app\assets\AppAsset::cla
 
 <header id="head">
     <div class="flex wrapper_nav">
-        <div class="nav" id="label">book room</div>
-        <div class="nav" id="arrow"><a href="#head" class="navigation"><span class="glyphicon">&#xe093;</span></a></div>
+        <div class="flex">
+            <div class="nav" id="back"><a href="/" class="navigation"><span class="glyphicon glyphicon-arrow-left"></span></a></div>
+            <div class="nav" id="label">book room</div>
+        </div>
+            <div class="nav" id="arrow"><a href="#head" class="navigation"><span class="glyphicon">&#xe093;</span></a></div>
     </div>
     <div class="header_right flex" id="logo">
         <img src="/img/logo.png" height="160px"/>
@@ -21,17 +24,101 @@ $this->registerCssFile('/css/room.css', ['depends' => [\app\assets\AppAsset::cla
 </header>
 <section id="rooms">
     <div class="flex rooms">
-        <div class="flex column">
+        <div class="flex column" id="room">
             <div class="room" id="photo">
                 <img src="/img/room/singolo.jpg" width="100%" />
             </div>
             <div class="flex room" id="label">
                 <?= Yii::t('app','single room') ?>
             </div>
+            <?= \yii\helpers\Html::a(
+                    Yii::t('app', 'book now'),
+                    \yii\helpers\Url::to(['room/book', 'type' => 'single']),
+                    ['class' => 'flex room', 'id' => 'book_button']) ?>
+            <div class="flex room" id="facilities">
+                <div class="flex column">
+                    <span class="glyphicon glyphicon-user"></span>
+                    <span class="quantity_of_guests">1 - 3 <?= Yii::t('app','persons') ?></span>
+                </div>
+                <div class="flex">
+                    <div class="flex column">
+                        <span class="glyphicon glyphicon-leaf"></span>
+                        <span class="quantity_of_guests"><?= Yii::t('app','bathtub') ?></span>
+                    </div>
+                    <div class="flex">
+                        <span class="glyphicon glyphicon-triangle-left"></span>
+                        <span class="glyphicon glyphicon-triangle-right"></span>
+                    </div>
+                    <div class="flex column">
+                        <span class="glyphicon glyphicon-fire"></span>
+                        <span class="quantity_of_guests"><?= Yii::t('app','shower') ?></span>
+                    </div>
+                </div>
+                <div class="flex column">
+                    <span class="glyphicon glyphicon-signal"></span>
+                    <span class="quantity_of_guests"><?= Yii::t('app','Wi-Fi') ?></span>
+                </div>
+            </div>
+        </div>
+        <div class="flex column" id="room">
+            <div class="room" id="photo">
+                <img src="/img/room/matrimoniale.jpg" width="100%" />
+            </div>
+            <div class="flex room" id="label">
+                <?= Yii::t('app','double room') ?>
+            </div>
+            <?= \yii\helpers\Html::a(
+                Yii::t('app', 'book now'),
+                \yii\helpers\Url::to(['room/book', 'type' => 'double']),
+                ['class' => 'flex room', 'id' => 'book_button']) ?>
+            <div class="flex room" id="facilities">
+                <div class="flex column">
+                    <span class="glyphicon glyphicon-user"></span>
+                    <span class="quantity_of_guests">2 - 3 <?= Yii::t('app','persons') ?></span>
+                </div>
+                <div class="flex">
+                    <div class="flex column">
+                        <span class="glyphicon glyphicon-leaf"></span>
+                        <span class="quantity_of_guests"><?= Yii::t('app','bathtub') ?></span>
+                    </div>
+                    <div class="flex">
+                        <span class="glyphicon glyphicon-triangle-left"></span>
+                        <span class="glyphicon glyphicon-triangle-right"></span>
+                    </div>
+                    <div class="flex column">
+                        <span class="glyphicon glyphicon-fire"></span>
+                        <span class="quantity_of_guests"><?= Yii::t('app','shower') ?></span>
+                    </div>
+                </div>
+                <div class="flex column">
+                    <span class="glyphicon glyphicon-signal"></span>
+                    <span class="quantity_of_guests"><?= Yii::t('app','Wi-Fi') ?></span>
+                </div>
+            </div>
+        </div>
+        <div class="flex column" id="room">
+            <div class="room" id="photo">
+                <img src="/img/room/matrimoniale.jpg" width="100%" />
+            </div>
+            <div class="flex room" id="label">
+                <?= Yii::t('app','family room') ?>
+            </div>
+            <?= \yii\helpers\Html::a(
+                Yii::t('app', 'book now'),
+                \yii\helpers\Url::to(['room/book', 'type' => 'family']),
+                ['class' => 'flex room', 'id' => 'book_button']) ?>
             <div class="flex room" id="facilities">
                 <div class="flex column">
                     <span class="glyphicon glyphicon-user"></span>
                     <span class="quantity_of_guests">3 - 5 <?= Yii::t('app','persons') ?></span>
+                </div>
+                <div class="flex column">
+                    <span class="glyphicon glyphicon-leaf"></span>
+                    <span class="quantity_of_guests"><?= Yii::t('app','bathtub') ?></span>
+                </div>
+                <div class="flex column">
+                    <span class="glyphicon glyphicon-signal"></span>
+                    <span class="quantity_of_guests"><?= Yii::t('app','Wi-Fi') ?></span>
                 </div>
             </div>
         </div>
